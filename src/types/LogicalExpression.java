6cca9477f015334c14e51fd4076eca59fa05db4a
Ege Erdal <ege-erdal101@hotmail.com>
2023-11-02 19:10:39 +0300

    feature: equality parsing

---
+++ src/types/LogicalExpression.java
@@ -0,0 +1,59 @@
+package types;
+
+public class LogicalExpression implements Expression {
+    private TypeEnum type;
+    private Token operator;
+    private Expression left;
+    private Expression right;
+
+    public LogicalExpression(TypeEnum type, Token operator, Expression left, Expression right) {
+        this.type = type;
+        this.operator = operator;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public TypeEnum getType() {
+        return this.type;
+    }
+
+    public void setType(TypeEnum type) {
+        this.type = type;
+    }
+
+    public Token getOperator() {
+        return this.operator;
+    }
+
+    public void setOperator(Token operator) {
+        this.operator = operator;
+    }
+
+    public Expression getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(Expression left) {
+        this.left = left;
+    }
+
+    public Expression getRight() {
+        return this.right;
+    }
+
+    public void setRight(Expression right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " type='" + getType() + "'" +
+            ", operator='" + getOperator() + "'" +
+            ", left='" + getLeft() + "'" +
+            ", right='" + getRight() + "'" +
+            "}";
+    }
+
+}
---
+++ src/types/literals/BooleanLiteral.java
@@ -0,0 +1,12 @@
+package types.literals;
+
+import types.TypeEnum;
+
+public class BooleanLiteral extends Literal{
+
+    public BooleanLiteral(TypeEnum type, String value) {
+        super(type, value);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
