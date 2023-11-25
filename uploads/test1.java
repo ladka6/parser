157c813f8a9362647d593ecaddcffd434e1cbf75
Ege Erdal <ege-erdal101@hotmail.com>
2023-11-25 20:27:05 +0300

    function, variable and class public|protected|private annotation added

--- uploads/test1.java
+++ uploads/test1.java
@@ -2,14 +2,26 @@
 import java.util.List;
 import java.a.a;
 
-class test1 {
+public class test1 {
+    private String x = "hello";
 
-    String getSomething() {
+    test1() {
+        int x = 1;
+        int y = 2;
+        int z = x + y;
+    }
+
+    private int a(int c) {
+        return 1;
+    }
+
+    public String getSomething() {
         if (true) {
             a a = 1;
         } else {
             int b = 2;
         }
+        int x = this.a(a);
         int s = 1 + 2;
         return "hello";
     }
